5ddc668d04b40760f4a078792d6f8f2008308c2b
nicholashaydensmith <thesmithdynasty@gmail.com>
2013-05-15 14:02:56 -0600

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,32 +1,54 @@
 //
-//  threads.c
+// threads.c
 //
 //
-//  Created by Scott Brandt on 5/6/13.
+// Created by Scott Brandt on 5/6/13.
 //
 //
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <ucontext.h>
+#include <signal.h>
 
 #define _XOPEN_SOURCE
 #include <ucontext.h>
 
 #define NUM_THREADS 16
+#define NUM_SLOTS 160
+
+typedef void (*sighandler_t)(int);
+
 static ucontext_t ctx[NUM_THREADS];
+static int scheduler[NUM_SLOTS];
 
 static void test_thread(void);
 static int thread = 0;
 void thread_exit(int);
 
+void thread_handler(int signum){
+   thread_yield();
+   printf("Timer expired!\n");
+}
+
 // This is the main thread
 // In a real program, it should probably start all of the threads and then wait for them to finish
 // without doing any "real" work
 int main(void) {
+    srand (time (NULL));
     printf("Main starting\n");
 
     printf("Main calling thread_create\n");
 
+    
+    struct itimerval itval;
+    itval.it_interval.tv_sec = 1;
+    itval.it_interval.tv_usec = 0;
+    itval.it_value = itval.it_interval;
+    setitimer(ITIMER_VIRTUAL,&itval,NULL); 
+    signal(SIGVTALRM,thread_handler);
+    
     // Create 15 more threads
     for (int i = 1; i < NUM_THREADS; i++) {
         thread_create(&test_thread);
@@ -37,14 +59,14 @@ int main(void) {
     // Reset the starting thread to thread 0
     thread = 0;
 
-    // Loop, doing a little work then yielding to the other thread
-    while(1) {
-        printf("Main calling thread_yield\n");
-
-        thread_yield();
-
-        printf("Main returned from thread_yield\n");
+    printf("Handing out lottery tickets to threads\n");
+    for (int i = 0; i < NUM_SLOTS; i++) {
+        scheduler[i] = rand() % NUM_THREADS;
+        printf ("%d\n", scheduler[i]);
     }
+
+    // Loop, doing a little work then yielding to the other thread
+    while(1);
 
     // We should never get here
     exit(0);
@@ -56,15 +78,7 @@ static void test_thread(void) {
     printf("In test_thread\n");
 
     // Loop, doing a little work then yielding to the other thread
-    while(1) {
-
-        printf("Test_thread calling thread_yield\n");
-
-        thread_yield();
-
-        printf("Test_thread returned from thread_yield\n");
-    }
-
+    while(1); 
     thread_exit(0);
 }
 
@@ -72,12 +86,8 @@ static void test_thread(void) {
 int thread_yield() {
     int old_thread = thread;
 
-    // This is the round round scheduler.
-    if (thread != NUM_THREADS - 1) {
-        thread++;
-    } else {
-        thread = 0;
-    }
+    // This is the lottery scheduler.
+    thread = scheduler[rand() % NUM_SLOTS];
 
     printf("Thread %d yielding to thread %d\n", old_thread, thread);
     printf("Thread %d calling swapcontext\n", old_thread);
